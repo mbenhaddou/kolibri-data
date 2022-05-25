32175a8c296ca19451e4f47e12f6165b1325b100
mohamedmentis <mbenhaddou@mentis-consulting.be>
2022-05-25 15:49:37 +0200

    upadare index.json

--- tools/build_packages.py
+++ tools/build_packages.py
@@ -14,25 +14,33 @@ import zipfile
 from kdmt.checksum import md5_hexdigest
 from kdmt.file import read_json_file
 
-def _path_from(parent, child):
+def _path_from(parent, child, base_dir=None):
     if os.path.split(parent)[1] == "":
         parent = os.path.split(parent)[0]
+
     path = []
     while parent != child:
         child, dirname = os.path.split(child)
         path.insert(0, dirname)
         assert os.path.split(child)[0] != child
+    if base_dir is not None:
+        path.insert(0,base_dir)
+
+    if len(path)>1:
+        return os.path.join(*path)
+    elif len(path)==1:
+        return path[0]
     return path
 
 
 def _get_sub_package_data(dirname, root, pkg_file):
 
     for f_root, d_names, f_names in os.walk(dirname):
-        content = {"path": "/".join(_path_from(root, f_root)), "files": []}
+        content = {"path": _path_from(root, f_root,base_dir="packages"), "files": []}
         for f in f_names:
             if f not in ["_package_.json",  pkg_file["name"], "_other_packages_.json", ".DS_Store"]:
                 content["files"].append(
-                    {"name": f, "path": "/".join(_path_from(root, os.path.join(f_root,f))), "checksum": md5_hexdigest(os.path.join(f_root,f)), "url": f"https://github.com/mbenhaddou/kolibri-data/blob/main/packages/{os.path.relpath(f_root, root)}/{f}", "unzip": False})
+                    {"name": f, "path": _path_from(root, os.path.join(f_root,f), base_dir="packages"), "checksum": md5_hexdigest(os.path.join(f_root,f)), "url": f"https://github.com/mbenhaddou/kolibri-data/blob/main/packages/{os.path.relpath(f_root, root)}/{f}", "unzip": False})
         if len(content["files"]) > 0:
             pkg_file["sub_packages"].append(content)
 
@@ -41,7 +49,7 @@ def _get_sub_package_data(dirname, root, pkg_file):
 def find_packages(root, recreate_packages=False):
 
     for dirname, subdirs, files in os.walk(root):
-        relpath = "/".join(_path_from(root, dirname))
+        relpath = _path_from(root, dirname)
 
         pkg_filename="_package_.json"
         other_pkg_filename = "_other_packages_.json"
